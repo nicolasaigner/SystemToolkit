5b72257ee2bab55bea73b71e723d74cee3dbc93a
Nicolas Aigner <nicolas.aigner@gmail.com>
2025-04-29 03:35:39 +0000

    Ajustando as pastas e correção dos scripts

--- NicolasAigner.SystemToolkit.psm1
+++
@@ -1,5 +0,0 @@
-# Auto‑loader: importa todas as funções do subdiretório Functions
-Get-ChildItem -Path (Join-Path $PSScriptRoot 'Functions') -Filter '*.ps1' | ForEach-Object {
-    . $_.FullName
-    Export-ModuleMember -Function $_.BaseName
-}
