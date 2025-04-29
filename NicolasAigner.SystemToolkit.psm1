fd07273f2b960f483656536f1e661795dca5df1d
Nicolas Aigner <nicolas.aigner@gmail.com>
2025-04-29 03:59:44 +0000

    Fix autoloader; split commands in Pester step

--- NicolasAigner.SystemToolkit.psm1
+++ NicolasAigner.SystemToolkit.psm1
@@ -1,5 +1,6 @@
 # Auto‑loader: importa todas as funções do subdiretório Functions
-Get-ChildItem -Path (Join-Path $PSScriptRoot 'Functions') -Filter '*.ps1' | ForEach-Object {
+Get-ChildItem -Path (Join-Path $PSScriptRoot 'Functions') -Filter '*.ps1' |
+ForEach-Object {
     . $_.FullName
     Export-ModuleMember -Function $_.BaseName
 }
